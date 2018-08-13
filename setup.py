636ddd45103afd869066b955f5452fa09fd1deaa
Matt Arcidy <marcidy@gmail.com>
2018-08-12 17:19:51 -0700

    now installable, and starting tests

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="donate",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+        'flask-migrate',
+        'flask-sqlalchemy',
+        'python-dotenv',
+        'werkzeug',
+    ],
+)
