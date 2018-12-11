a0ab3759b3299140e6896b403a819f0177faaa3c
Matt Arcidy <marcidy@gmail.com>
2018-12-10 16:29:29 -0800

    adding widgets/for users

---
+++ donate/widgets/users.py
@@ -0,0 +1,21 @@
+from ..models import User
+from ..extensions import db
+
+
+def add_new_user(data):
+    email = data['email']
+    slack = data['slack']
+    username = data['username']
+
+    # can't test this query without a database, time to create a dev env
+    folks = db.session.query(User).filter((User.username == username) |
+                                          (User.slack == slack) |
+                                          (User.email == email)).count()
+
+    if folks > 0:
+        raise ValueError("User {} already exists!".format(username))
+
+    db.session.add(User(username=username,
+                        slack=slack,
+                        email=email))
+    db.session.commit()
