67abe71b6db1e0f253ba1fc0ebab6ee91669b02a
Matt Arcidy <marcidy@gmail.com>
2019-05-20 20:00:24 -0700

    initializing logger config, nominally working

--- autoapp.py
+++ autoapp.py
@@ -1,9 +1,12 @@
 from flask.helpers import get_debug_flag
+import logging
 from donate.app import create_app
 from donate.settings import DevConfig, ProdConfig
 import os
 
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
-
+logging.basicConfig(filename=CONFIG.LOG_FILE,
+                    format=CONFIG.LOG_FORMAT,
+                    level=CONFIG.LOG_LEVEL)
 app = create_app(CONFIG)
 app.secret_key = os.environ['DONATE_SECRET']
