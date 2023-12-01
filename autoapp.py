2cc0fc65ee8da595207eccb8759c71fc5bef39d2
Dan H <danhimalplanet@gmail.com>
2023-12-01 13:22:24 -0800

    flask loads vars from .env without this line.   flask fails if this line is not commented out.  not sure whats going on.  seems like the right thing to do

--- autoapp.py
+++ autoapp.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 import os
-load_dotenv(os.environ['DONATE_DOTENV'])
+#load_dotenv(os.environ['DONATE_DOTENV'])
 
 from donate.app import create_app
 from donate.database import db
